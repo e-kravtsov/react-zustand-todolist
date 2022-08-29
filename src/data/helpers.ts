2ba1fd2a3206445892fa590393f8d7478cc68deb
Egor Kravtsov <egorka0192@gmail.com>
2022-08-29 10:46:34 +0300

    using built in persist

--- src/data/helpers.ts
+++ src/data/helpers.ts
@@ -1,4 +1,13 @@
+import Cookies from 'js-cookie'
+
 type GenerateId = () => string;
 export const generateId: GenerateId = () => (
     Math.random().toString(16).slice(2) + new Date().getTime().toString(36)
 );
+
+
+export const cookiesStorage = {
+  getItem: (name: string) => Cookies.get(name)||'',
+  setItem: (name: string, value: string) =>  {Cookies.set(name, value)},
+  removeItem: (name: string) => Cookies.remove(name)
+}
--- src/data/stores/useToDoStore.ts
+++ src/data/stores/useToDoStore.ts
@@ -1,9 +1,9 @@
 import create, {State, StateCreator} from 'zustand'
 import {devtools} from 'zustand/middleware'
-import {generateId} from '../helpers'
-import Cookies from 'js-cookie'
+import {generateId, cookiesStorage} from '../helpers'
+import { persist } from 'zustand/middleware'
+// import Cookies from 'js-cookie'
 
-Cookies.set('foo', 'bar')
 
 interface Task {
     id: string;
@@ -22,28 +22,31 @@ function isToDoStore(object: any): object is ToDoStore{
     return 'tasks' in object;
 }
 
-const localStorageUpdate =  <T extends State>(config: StateCreator<T>)
-: StateCreator<T>=>(set, get, api) => config((nextState, ...args)=>{
-    if(isToDoStore(nextState)){
-        Cookies.set('tasks', JSON.stringify(nextState.tasks), { expires: 7 })
-    }
-    set(nextState, ...args)
-}, get, api)
+/*
+    here is my own middleware implementation
+*/
+// const localStorageUpdate =  <T extends State>(config: StateCreator<T>)
+// : StateCreator<T>=>(set, get, api) => config((nextState, ...args)=>{
+//     if(isToDoStore(nextState)){
+//         Cookies.set('tasks', JSON.stringify(nextState.tasks), { expires: 7 })
+//     }
+//     set(nextState, ...args)
+// }, get, api)
 
-const getCurrentState = ():Task[] => {
-    try{
-    return JSON.parse(Cookies.get('tasks')||'[]') as Task[]
-    } catch(error){
-        console.log(error)
-        return []
-    }
-}
+// const getCurrentState = ():Task[] => {
+//     try{
+//     return JSON.parse(Cookies.get('tasks')||'[]') as Task[]
+//     } catch(error){
+//         console.log(error)
+//         return []
+//     }
+// }
 
 export const useToDoStore = create<ToDoStore>(
-    localStorageUpdate(
+    persist(
         devtools(
             (set, get)=>({
-                tasks: getCurrentState(),
+                tasks: [],
                 createTask:(title)=>{
                     const {tasks}=get();
                     const task: Task = {
@@ -69,6 +72,12 @@ export const useToDoStore = create<ToDoStore>(
                     });
                 }
             })
-        )
+        ),
+        {
+            name: 'tasks',
+            getStorage: () => cookiesStorage,
+            serialize: (state) => btoa(JSON.stringify(state)),
+            deserialize: (str) => JSON.parse(atob(str)),
+        }
     )
 )
